9899be2a4a4a6e7d083179651a4506067d8e4e1b
bkjung <bkjung94@gmail.com>
2020-01-16 13:25:57 +0900

    working on frame based modification

--- AAL_check_program.py
+++ AAL_check_program.py
@@ -1,5 +1,5 @@
 import csv
-import tkinter as tk
+from tkinter import *
 import numpy as np
 import math
 import numbers
@@ -16,47 +16,68 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.debug("Coordinate Logging Start")
 
-root = tk.Tk()
+root = Tk()
+#root.maxsize(500, 500) 
+root.wm_title("AAL Check Program")
 
-root.title("AAL Check Program")
+input_frame = LabelFrame(root, text="Input in World (Voxel) Coordinates") 
+input_frame.grid(row=1, columnspan=7, sticky='W', padx=5, pady=5, ipadx=5, ipady=5)
+button_frame = LabelFrame(root, text="Buttons")
+button_frame.grid(row=2, columnspan=7, sticky='W', padx=5, pady=5, ipadx=5, ipady=5)
+result_frame = LabelFrame(root, text="Results")
+result_frame.grid(row=3, columnspan=7, sticky='W', padx=5, pady=5, ipadx=5, ipady=5)
 
-canvas1 = tk.Canvas(root, width=500, height=500)
-canvas1.pack()
+#Label(input_frame, text="Input in World (Voxel) Coordinates").grid(row=0, column=0, padx=5, pady=5)
 
-entry1 = tk.Entry(root)
-canvas1.create_window(210, 100, window=entry1)
+#canvas1 = Canvas(root, width=500, height=500)
+#canvas1.pack()
 
-entry2 = tk.Entry(root)
-canvas1.create_window(210, 140, window=entry2)
+#entry1 = Entry(root)
+#canvas1.create_window(210, 100, window=entry1)
 
-entry3 = tk.Entry(root)
-canvas1.create_window(210, 180, window=entry3)
+#entry2 = Entry(root)
+#canvas1.create_window(210, 140, window=entry2)
 
-label0 = tk.Label(root, text='Coordinate - AAL check')
-label0.config(font=('helvetica', 20))
-canvas1.create_window(200, 40, window=label0)
+#entry3 = Entry(root)
+#canvas1.create_window(210, 180, window=entry3)
 
-label1 = tk.Label(root, text='Z:')
-label1.config(font=('helvetica', 14))
-canvas1.create_window(100, 100, window=label1)
+label0 = Label(root, text='AAL Brain Region Check')
+#label0.config(font=('helvetica', 20))
+label0.grid(row=0, column=0, sticky='E', padx=5, pady=2)
+#canvas1.create_window(200, 40, window=label0)
 
-label2 = tk.Label(root, text='X:')
-label2.config(font=('helvetica', 14))
-canvas1.create_window(100, 140, window=label2)
+labelInput = Label(root, text='Input in World (Voxel) Coordinate')
+labelInput.config(font=('helvetica', 12))
+#canvas1.create_window(200, 70, window=labelInput)
 
-label3 = tk.Label(root, text='Y:')
-label3.config(font=('helvetica', 14))
-canvas1.create_window(100, 180, window=label3)
+#label1 = Label(root, text='Z:')  
+#label1.config(font=('helvetica', 14))
+#canvas1.create_window(100, 100, window=label1)
 
-v = tk.StringVar()
-label4 = tk.Label(root, textvariable = v, font=(
+label_Z = Label(input_frame, text="Z:")
+label_Z.grid(row=1, column=0, sticky='E', padx=5, pady=2)
+#label1 = Label(input_frame, text='Z:')  
+#label1.config(font=('helvetica', 14))
+#label1.place(x=100, y=100)
+
+label_X = Label(input_frame, text='X:')
+label_X.grid(row=2, column=0, sticky='E', padx=5, pady=2)
+#canvas1.create_window(100, 140, window=label2)
+
+label_Y = Label(input_frame, text='Y:')
+label_Y.grid(row=2, column=0, sticky='E', padx=5, pady=2)
+#label_Y.config(font=('helvetica', 14))
+#canvas1.create_window(100, 180, window=label3)
+
+v = StringVar()
+label4 = Label(root, textvariable = v, font=(
         'helvetica', 14, 'bold'), bg='white', width=25)
-canvas1.create_window(210, 300, window=label4)
-label4.pack()
+#canvas1.create_window(210, 300, window=label4)
+#label4.pack()
 
-label5 = tk.Label(root, text='Result:')
+label5 = Label(root, text='Result:')
 label5.config(font=('helvetica', 14))
-canvas1.create_window(80, 300, window=label5)
+#canvas1.create_window(80, 300, window=label5)
 
 with open('data/coordinate_list.csv', 'r') as f:
     reader = csv.reader(f)
@@ -159,7 +180,7 @@ def check():
     #print("INPUT nifti coordinate : ", x, y, z)
     AAL = nifti_to_region_name(x, y, z)
 
-    #label4 = tk.Label(root, text=AAL, font=(
+    #label4 = Label(root, text=AAL, font=(
     #    'helvetica', 14, 'bold'), bg='white')
     #canvas1.create_window(210, 300, window=label4)
     v.set(AAL)
@@ -171,9 +192,9 @@ def check():
 #             f(*args, **kwargs)
 #     return combined_func
 
-buttonCheck = tk.Button(text='Check', command=check, bg='green',
+buttonCheck = Button(text='Check', command=check, bg='green',
                         fg='white', font=('helvetica', 12, 'bold'), width=5)
-canvas1.create_window(210, 240, window=buttonCheck)
+#canvas1.create_window(210, 240, window=buttonCheck)
 
 def reset():
     entry1.delete(0, 'end')
@@ -181,14 +202,14 @@ def reset():
     entry3.delete(0, 'end')
     v.set("")
 
-buttonReset = tk.Button(text='Reset', command=reset, bg='green',
+buttonReset = Button(text='Reset', command=reset, bg='green',
                         fg='white', font=('helvetica', 12, 'bold'), width=5)
 
-canvas1.create_window(210, 300, window=label4)
+#canvas1.create_window(210, 300, window=label4)
 
-#buttonReset = tk.Button(text='Reset', command=reset, bg='green',
+#buttonReset = Button(text='Reset', command=reset, bg='green',
 #                        fg='white', font=('helvetica', 12, 'bold'), width=5)
                         
-canvas1.create_window(400, 120, window=buttonReset)
+#canvas1.create_window(400, 120, window=buttonReset)
 
 root.mainloop()
